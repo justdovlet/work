630600045b27c28cab565a767311ec78f2ea4cc1
justdovlet <69221795+justdovlet@users.noreply.github.com>
2021-11-13 22:31:27 +0300

    Update simpleid.c

--- 2021-2022/Информационная безопасность/laboratory/simpleid.c
+++ 2021-2022/Информационная безопасность/laboratory/simpleid.c
@@ -1,15 +1,21 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h>
 int
-main ()
+main (int argc, char* argv[])
 {
-uid_t real_uid = getuid ();
-uid_t e_uid = geteuid ();
-gid_t real_gid = getgid ();
-gid_t e_gid = getegid () ;
-printf ("e_uid=%d, e_gid=%d\n", e_uid, e_gid);
-printf ("real_uid=%d, real_gid=%d\n", real_uid,
-,→ real_gid);
+unsigned char buffer[16];
+size_t bytes_read;
+int i;
+int fd = open (argv[1], O_RDONLY);
+do
+{
+bytes_read = read (fd, buffer, sizeof (buffer));
+for (i =0; i < bytes_read; ++i) printf("%c", buffer[i]);
+}
+while (bytes_read == sizeof (buffer));
+close (fd);
 return 0;
 }
