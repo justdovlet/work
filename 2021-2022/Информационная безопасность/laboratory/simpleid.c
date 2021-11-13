64f35be54b4304577c4a1f813e2498e2d244117a
justdovlet <69221795+justdovlet@users.noreply.github.com>
2021-11-13 22:27:39 +0300

    Update simpleid.c

--- 2021-2022/Информационная безопасность/laboratory/simpleid.c
+++ 2021-2022/Информационная безопасность/laboratory/simpleid.c
@@ -1,13 +1,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-
 int
 main ()
-
 {
-uid_t uid = geteuid ();
-gid_t gid = getegid ();
-printf ("uid=%d, gid=%d\n", uid, gid);
+uid_t real_uid = getuid ();
+uid_t e_uid = geteuid ();
+gid_t real_gid = getgid ();
+gid_t e_gid = getegid () ;
+printf ("e_uid=%d, e_gid=%d\n", e_uid, e_gid);
+printf ("real_uid=%d, real_gid=%d\n", real_uid,
+,→ real_gid);
 return 0;
 }
